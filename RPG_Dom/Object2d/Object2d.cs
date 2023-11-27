db7807f65587d2c361866fdc69f5ccff0bbc69a4
Dominic Senarsky <dominico.sebastian@gmail.com>
2023-11-27 23:42:22 +0100

    Added comments

--- RPG_Dom/Object2d/Object2d.cs
+++ RPG_Dom/Object2d/Object2d.cs
@@ -42,9 +42,11 @@ namespace RPG_Dom
         }
 
         public virtual void Draw()
-        {
+        { 
+
             // Draw the selected model as a rectangle for hitbox and rotation measures
-            Globals.spriteBatch.Draw(myObject, new Microsoft.Xna.Framework.Rectangle((int)(pos.X), (int)(pos.Y), (int)(dims.X), (int)(dims.Y)), null, Microsoft.Xna.Framework.Color.White, Globals.playerRotation, new Vector2(myObject.Bounds.Width / 2, myObject.Bounds.Height / 2), new SpriteEffects(), 0);
+            Globals.spriteBatch.Draw(myObject, new Microsoft.Xna.Framework.Rectangle((int)(pos.X), (int)(pos.Y), (int)(dims.X), (int)(dims.Y)), null, Microsoft.Xna.Framework.Color.White, 0f, new Vector2(myObject.Bounds.Width / 2, myObject.Bounds.Height / 2), new SpriteEffects(), 0);
+            // FIGURE OUT HOW TO CHANGE ROTATE SEPERATELY FOR EACH SPRITE PASSED
         }
 
     }
