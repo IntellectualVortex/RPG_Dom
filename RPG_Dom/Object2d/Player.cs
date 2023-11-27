5211657d2d8b737e85ee982bce39ead2bd13d036
Dominic Senarsky <dominico.sebastian@gmail.com>
2023-11-27 12:13:32 +0100

    Add project files.

---
+++ RPG_Dom/Object2d/Player.cs
@@ -0,0 +1,29 @@
+﻿#region Includes
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+#endregion
+
+namespace RPG_Dom.Object2d
+{
+    internal class Player
+    {
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+}
---
+++ RPG_Dom/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new RPG_Dom.Game1();
+game.Run();
